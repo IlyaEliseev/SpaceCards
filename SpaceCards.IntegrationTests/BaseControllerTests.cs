779a8b1e49772b6a0c3018b5512aa365b71d5a89
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-05-06 13:08:41 +0300

    wip: edit integrations tests

---
+++ SpaceCards.IntegrationTests/BaseControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using SpaceCards.API.Contracts;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpaceCards.IntegrationTests
+{
+    public abstract class BaseControllerTests
+    {
+        protected readonly HttpClient _client;
+        protected readonly Fixture _fixture;
+
+        public BaseControllerTests()
+        {
+            var app = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureAppConfiguration((_, configurationBuilder) =>
+                    {
+                        configurationBuilder.AddUserSecrets(typeof(BaseControllerTests).Assembly);
+                    });
+                });
+            _client = app.CreateClient();
+            _fixture = new Fixture();
+        }
+    }
+}
