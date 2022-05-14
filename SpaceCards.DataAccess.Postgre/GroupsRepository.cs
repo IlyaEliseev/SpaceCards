2ebe640dd079b785ade8075d43d9a0a672487b4b
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-05-14 12:25:59 +0300

    wip: add null check in delete method and remove GetByIdWithCards methode (GroupRepository)

--- SpaceCards.DataAccess.Postgre/GroupsRepository.cs
+++ SpaceCards.DataAccess.Postgre/GroupsRepository.cs
@@ -15,13 +15,13 @@ namespace SpaceCards.DataAccess.Postgre
             _mapper = mapper;
         }
 
-        public async Task<(int Result, string[] Errors)> Add(Group group)
+        public async Task<int> Add(Group group)
         {
             var groupEntity = _mapper.Map<Domain.Group, Entites.Group>(group);
             await _context.Groups.AddAsync(groupEntity);
             await _context.SaveChangesAsync();
 
-            return (groupEntity.Id, Array.Empty<string>());
+            return groupEntity.Id;
         }
 
         public async Task<Group[]> Get()
@@ -50,14 +50,21 @@ namespace SpaceCards.DataAccess.Postgre
             await _context.SaveChangesAsync();
         }
 
-        public async Task Delete(int groupId)
+        public async Task<bool> Delete(int groupId)
         {
             var group = await _context.Groups
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.Id == groupId);
 
-            _context.Groups.Remove(new Entites.Group { Id = group.Id });
+            if (group is null)
+            {
+                return false;
+            }
+
+            _context.Groups.Remove(new Entites.Group { Id = groupId });
             await _context.SaveChangesAsync();
+
+            return true;
         }
 
         public async Task<bool> AddCard(int cardId, int groupId)
@@ -65,22 +72,16 @@ namespace SpaceCards.DataAccess.Postgre
             var card = await _context.Cards
                 .FirstOrDefaultAsync(x => x.Id == cardId);
 
+            if (card is null)
+            {
+                return false;
+            }
+
             card.GroupId = groupId;
 
             await _context.SaveChangesAsync();
 
             return true;
-        }
-
-        public async Task<Group?> GetByIdWithCards(int groupId)
-        {
-            var group = await _context.Groups
-                .AsNoTracking()
-                .Include(x => x.Cards)
-                .Where(x => x.Cards.Count != 0)
-                .FirstOrDefaultAsync(x => x.Id == groupId);
-
-            return _mapper.Map<Entites.Group, Domain.Group>(group);
         }
     }
 }
