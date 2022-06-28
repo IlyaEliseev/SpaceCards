608425c5eb03a2d02f288ea586b3701cee70ad1a
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-06-28 14:31:58 +0300

    wip: email statistics

---
+++ backend/SpaceCards.StatisticsReporter/Program.cs
@@ -0,0 +1,10 @@
+using SpaceCards.StatisticsReporter;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
