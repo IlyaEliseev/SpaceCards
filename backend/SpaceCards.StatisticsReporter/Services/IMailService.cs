bb5e19de2ea7499b389bc9d8d9c958969c0eaf5a
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-07-02 19:34:12 +0300

    feat: email statistics

---
+++ backend/SpaceCards.StatisticsReporter/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using SpaceCards.StatisticsReporter.Models;
+
+namespace SpaceCards.StatisticsReporter.Services
+{
+    public interface IMailService
+    {
+        Task SendEmail(MailRequest mailRequest);
+    }
+}
---
+++ backend/SpaceCards.StatisticsReporter/Settings/MailSettingsOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceCards.StatisticsReporter.Settings
+{
+    public class MailSettingsOptions
+    {
+        public const string MailSettings = "MailSettings";
+
+        public string Mail { get; set; } = string.Empty;
+
+        public string DisplayName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Host { get; set; } = string.Empty;
+
+        public int Port { get; set; }
+    }
+}
